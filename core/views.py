e5fa7f757c29695a529ac3119453b4dc0a21a3cc
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-22 12:28:32 +0800

    Overriding the Behaviour of List Method

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Customer, Profession, DataSheet, Document
 from .serializers import (
@@ -15,17 +16,23 @@ from .serializers import (
 class CustomerViewSet(viewsets.ModelViewSet):
     serializer_class = CustomerSerializer
 
-    #? Override Method get_queryset
+    # ? Override Method get_queryset
     def get_queryset(self):
-        # import pdb; pdb.set_trace() #? Create Breakpoints
+        # import pdb; pdb.set_trace() # ? Create Breakpoints
 
         active_customers = Customer.objects.filter(active=True)
         return active_customers
-    
+
+    # ? Override List method behaviour
+    def list(self, request, *args, **kwargs):
+        # import pdb; pdb.set_trace() # ? Create Breakpoints
+        customers = Customer.objects.filter(id=3)
+        serializer = CustomerSerializer(customers, many=True)
+        return Response(serializer.data)
 
 
 class ProfessionViewSet(viewsets.ModelViewSet):
-    # queryset = Profession.objects.filter(id=2) #? Filter only id=2 will be shown
+    # queryset = Profession.objects.filter(id=2) # ? Filter only id=2 will be shown
     queryset = Profession.objects.all()
     serializer_class = ProfessionSerializer
 
