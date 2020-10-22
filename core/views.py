7d2c03a9a810671e9cb6b36e10dee2f4d51ed0f0
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-22 15:13:04 +0800

    Override the Behaviour of PUT Method

--- core/views.py
+++ core/views.py
@@ -32,22 +32,44 @@ class CustomerViewSet(viewsets.ModelViewSet):
         serializer = CustomerSerializer(customers, many=True)
         return Response(serializer.data)
 
+    # ? Override Retrieve method behaviour
     def retrieve(self, request, *args, **kwargs):
         # return HttpResponseForbidden('Not Allowed') # ? Response Manual
         obj = self.get_object()
         serializer = CustomerSerializer(obj)
         return Response(serializer.data)
 
+    # ? Override Create method behaviour
     def create(self, request, *args, **kwargs):
         data = request.data
         print(data)
         customer = Customer.objects.create(
-            name = data['name'],
-            address = data['address'],
-            data_sheet_id = data['data_sheet'],
+            name=data['name'],
+            address=data['address'],
+            data_sheet_id=data['data_sheet'],
             # profession = data['profession']
         )
         profession = Profession.objects.get(id=data['professions'])
+        customer.professions.add(profession)
+        customer.save()
+
+        serializer = CustomerSerializer(customer)
+        return Response(serializer.data)
+
+    # ? Override Update method behaviour
+    def update(self, request, *args, **kwargs):
+        customer = self.get_object()
+        data = request.data
+
+        customer.name = data['name']
+        customer.address = data['address']
+        customer.data_sheet_id = data['data_sheet']
+
+        profession = Profession.objects.get(id=data['professions'])
+
+        for p in customer.professions.all():
+            customer.professions.remove(p)
+
         customer.professions.add(profession)
         customer.save()
 
