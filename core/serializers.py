7887215f875a423e501af52d90bb3368ee0c89fc
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-23 12:00:31 +0800

    Taking Advantage of Nested Serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,15 +1,27 @@
 from rest_framework import serializers
 from .models import Customer, Profession, DataSheet, Document
 
-# Serializers define the API representation.
+
+class DataSheetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DataSheet
+        fields = ['id', 'description', 'historical_data']
+
+
+class ProfessionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profession
+        fields = ['id', 'description']
 
 
 class CustomerSerializer(serializers.ModelSerializer):
     num_professions = serializers.SerializerMethodField()
     # data_sheet = serializers.SerializerMethodField()
     # data_sheet = serializers.StringRelatedField() # ? Example of StringRelatedField
-    data_sheet = serializers.PrimaryKeyRelatedField(read_only=True)
-    professions = serializers.StringRelatedField(many=True)
+    # data_sheet = serializers.PrimaryKeyRelatedField(read_only=True) # ? Example of PrimaryKeyRelatedField
+    data_sheet = DataSheetSerializer()  # ? Nested Serializer
+    # professions = serializers.StringRelatedField(many=True)
+    professions = ProfessionSerializer(many=True)
     document_set = serializers.StringRelatedField(many=True)
 
     class Meta:
@@ -22,18 +34,6 @@ class CustomerSerializer(serializers.ModelSerializer):
 
     # def get_data_sheet(self, obj):
     #     return obj.data_sheet.description
-
-
-class ProfessionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profession
-        fields = ['id', 'description']
-
-
-class DataSheetSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = DataSheet
-        fields = ['id', 'description', 'historical_data']
 
 
 class DocumentSerializer(serializers.ModelSerializer):
