073da391d15e150e311d066a80a7435deb8ef6f7
Frastyawan Nym <frastyawan.nym@gmail.com>
2020-10-23 14:54:15 +0800

    DjangoModelPermissions & DjangoModelPermissionsOrAnonReadOnly

--- core/views.py
+++ core/views.py
@@ -4,7 +4,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import AllowAny, IsAdminUser, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import (
+    AllowAny,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+    DjangoModelPermissions,
+    DjangoModelPermissionsOrAnonReadOnly
+)
 from django.http.response import HttpResponseForbidden
 from django_filters.rest_framework import DjangoFilterBackend
 
@@ -25,14 +31,16 @@ class CustomerViewSet(viewsets.ModelViewSet):
     filterset_fields = ['name']  # ? Filtering Search
     ordering_fields = ['id', 'name']  # ? Ordering Search
     # ordering_fields = '__all__' # ? Ordering by All Fields
-    ordering = ['-id'] # ? Default Ordering
-    search_fields = ['name', 'address', 'data_sheet__description', ]  # ? Field Filtered
+    ordering = ['-id']  # ? Default Ordering
+    search_fields = ['name', 'address',
+                     'data_sheet__description', ]  # ? Field Filtered
     # search_fields = ['=name'] # ? iexact
     # search_fields = ['^name'] # ? istartswith
 
-    lookup_field = 'code' # ? Default is id, ex: http://127.0.0.1:8000/api/customers/3/. Now like this: http://127.0.0.1:8000/api/customers/CS001/
+    lookup_field = 'code'  # ? Default is id, ex: http://127.0.0.1:8000/api/customers/3/. Now like this: http://127.0.0.1:8000/api/customers/CS001/
 
-    authentication_classes = [TokenAuthentication,] # ? Authentication Settings
+    # ? Authentication Settings
+    authentication_classes = [TokenAuthentication, ]
 
     # ? Override Method get_queryset
     def get_queryset(self):
@@ -164,8 +172,8 @@ class CustomerViewSet(viewsets.ModelViewSet):
 
 
 class ProfessionViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication,]
-    permission_classes = [IsAdminUser,]
+    authentication_classes = [TokenAuthentication, ]
+    permission_classes = [IsAdminUser, ]
 
     # queryset = Profession.objects.filter(id=2) # ? Filter only id=2 will be shown
     queryset = Profession.objects.all()
@@ -173,15 +181,17 @@ class ProfessionViewSet(viewsets.ModelViewSet):
 
 
 class DataSheetViewSet(viewsets.ModelViewSet):
-    permission_classes = [AllowAny,] # ? Permission Settings
+    permission_classes = [AllowAny, ]  # ? Permission Settings
 
     queryset = DataSheet.objects.all()
     serializer_class = DataSheetSerializer
 
 
 class DocumentViewSet(viewsets.ModelViewSet):
-    authentication_classes = [TokenAuthentication,]
-    permission_classes = [IsAuthenticatedOrReadOnly,]
+    authentication_classes = [TokenAuthentication, ]
+    # permission_classes = [IsAuthenticatedOrReadOnly, ]
+    # permission_classes = [DjangoModelPermissions, ]
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly, ]
 
     queryset = Document.objects.all()
     serializer_class = DocumentSerializer
